f261febcd9cc50abf0199f994c07ba930f6d56c1
Kristian <kpskola1002@gmail.com>
2023-02-25 14:27:20 +0100

    Add support of auth, deauth paths

--- app/routes/web.mjs
+++ app/routes/web.mjs
@@ -3,6 +3,7 @@ import { Router } from "../classes/Router.mjs";
 import { View } from "../classes/View.mjs";
 
 Router.init();
+
 Router.get("/", (res) => {
   return new View("index", {
     title: "Instagram Photos downloader",
@@ -10,13 +11,25 @@ Router.get("/", (res) => {
   });
 });
 
-Router.get('/home', (res) => {
-  return new View('index', {
-      title: 'Redirected',
-      showButton: false,
-  })
-})
+Router.get("/auth/", ({ code }, res) => {
+  console.log(code);
 
-Router.post("/fetch-instagram-token", (res) => {
-  return new Redirect("/home");
+  return new View("index", {
+    title: "Thank you for using this app",
+    showButton: false,
+  });
+});
+
+Router.get("/deauth/", ({ code }, res) => {
+  console.log(code);
+
+  return new View("index", {
+    title: "We are sorry to hear, that you don't want to use the app",
+    showButton: false,
+  });
+});
+
+Router.post("/fetch-instagram-token", () => {
+  const instagramOathPath = `${global.instagram.INSTAGRAM_BASE_PATH}/authorize?client_id=${global.instagram.CLIENT_ID}&redirect_uri=${global.app.FULL_PATH}/auth/&response_type=code&scope=user_profile,user_media`;
+  return new Redirect(instagramOathPath, 301);
 });
